eef938c54594b6ccba705e43c9e8c7823357de0a
dbafromthecold <dbafromthecold@gmail.com>
2022-06-02 09:36:23 +0100

    updated demo

--- pod_eviction_test.sh
+++ pod_eviction_test.sh
@@ -1,46 +1,75 @@
+############################################################################
+############################################################################
+#
+# Kubernets Pod Eviction - Andrew Pruski
+# @dbafromthecold
+# dbafromthecold@gmail.com
+# https://github.com/dbafromthecold/KubernetesPodEviction
+#
+############################################################################
+############################################################################
 
 
 
+# log into Azure
 az login
 
 
 
+# create resource group
 az group create --name kubernetes --location EASTUS
 
 
 
+# create AKS cluster
 az aks create --name kubernetes1 --resource-group kubernetes --node-count 3 --generate-ssh-keys
 
 
 
+# get credentials to connect to cluster
 az aks get-credentials --name kubernetes1 --resource-group kubernetes
 
 
 
-az aks nodepool list --cluster-name kubernetes1 --resource-group kubernetes
+# view AKS nodepool
+az aks nodepool list --cluster-name kubernetes1 --resource-group kubernetes -o table
 
 
 
 # https://docs.microsoft.com/en-us/cli/azure/vmss?view=azure-cli-latest#az-vmss-list
-az vmss list --resource-group MC_kubernetes_kubernetes1_eastus
-az vmss deallocate --instance-ids 6 --name aks-nodepool1-96936133-vmss --resource-group MC_kubernetes_kubernetes1_eastus
-az vmss start --instance-ids 6 --name aks-nodepool1-96936133-vmss --resource-group MC_kubernetes_kubernetes1_eastus
+# list vmss
+az vmss list --resource-group MC_kubernetes_kubernetes1_eastus -o table
 
 
 
-kubectl get nodes
+# view AKS nodes
+kubectl get nodes -o jsonpath="{.items[0].metadata.name}"
 
 
 
+# deploy test application
 kubectl create deployment test --image=nginx
 
 
 
+# view deployment
 kubectl get all
 
 
 
+# view pods
 kubectl get pods -o wide --watch
+
+
+
+# switch to new terminal
+# shutdown node in AKS
+az vmss deallocate --instance-ids 6 --name aks-nodepool1-96936133-vmss --resource-group MC_kubernetes_kubernetes1_eastus
+
+
+
+# restart node in AKS
+az vmss start --instance-ids 6 --name aks-nodepool1-96936133-vmss --resource-group MC_kubernetes_kubernetes1_eastus
 
 
 
@@ -60,3 +89,11 @@ kubectl edit deployment test
 
 
 
+# switch to other terminal
+# shutdown node in AKS
+az vmss deallocate --instance-ids 6 --name aks-nodepool1-96936133-vmss --resource-group MC_kubernetes_kubernetes1_eastus
+
+
+
+# restart node in AKS
+az vmss start --instance-ids 6 --name aks-nodepool1-96936133-vmss --resource-group MC_kubernetes_kubernetes1_eastus
