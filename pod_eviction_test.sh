b00c184b6a760d8ed86e1d4168f65665d2d781b0
dbafromthecold <dbafromthecold@gmail.com>
2022-06-02 09:21:20 +0100

    updated demo

--- pod_eviction_test.sh
+++ pod_eviction_test.sh
@@ -1,33 +1,51 @@
 
 
+
 az login
+
 
 
 az group create --name kubernetes --location EASTUS
 
 
+
 az aks create --name kubernetes1 --resource-group kubernetes --node-count 3 --generate-ssh-keys
+
 
 
 az aks get-credentials --name kubernetes1 --resource-group kubernetes
 
 
+
 az aks nodepool list --cluster-name kubernetes1 --resource-group kubernetes
+
+
+
+# https://docs.microsoft.com/en-us/cli/azure/vmss?view=azure-cli-latest#az-vmss-list
+az vmss list --resource-group MC_kubernetes_kubernetes1_eastus
+az vmss deallocate --instance-ids 6 --name aks-nodepool1-96936133-vmss --resource-group MC_kubernetes_kubernetes1_eastus
+az vmss start --instance-ids 6 --name aks-nodepool1-96936133-vmss --resource-group MC_kubernetes_kubernetes1_eastus
+
 
 
 kubectl get nodes
 
 
+
 kubectl create deployment test --image=nginx
+
 
 
 kubectl get all
 
 
+
 kubectl get pods -o wide --watch
 
 
+
 kubectl edit deployment test
+
 
 
       tolerations:
